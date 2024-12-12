aca0d405b7228c5ac4138b6f82b254e97f700a43
Celeste Sánchez Fresneda <csanchez@infoavan.com>
2024-12-12 21:20:16 +0100

    First version, almost finished

---
+++ src/demo/Models/Prompts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Models;
+public class Prompts
+{
+    public string Mistral { get; set; }
+
+    public string Phi3 { get; set; }
+
+    public string Llama { get; set; }
+}
--- src/demo/Program.cs
+++ src/demo/Program.cs
@@ -2,40 +2,54 @@
 #pragma warning disable SKEXP0010
 #pragma warning disable SKEXP0050
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
-const string modelPhi3 = @"phi3:medium";
-const string endpointOllama = "http://localhost:11434";
+
+#region Get Configuration Values
+
+var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("prompts.json", optional:false);
+
+IConfiguration config = builder.Build();
+var configValues = config.GetSection("ModelConfiguration").Get<Demo.Models.Config>();
+
+var promptsValues = config.GetSection("prompts").Get<Demo.Models.Prompts>();
+
+#endregion
 
 var kernelBuilderMistral = Kernel.CreateBuilder()
-                                 .AddOpenAIChatCompletion(modelId: modelPhi3, endpoint: new Uri(endpointOllama), apiKey: null, serviceId: modelPhi3)
-                                 ;
+                                 .AddOpenAIChatCompletion(modelId: configValues.ModelId, endpoint: new Uri(configValues.Endpoint), apiKey: configValues.ApiKey);
+
 
 var kernelBuilderLlama = Kernel.CreateBuilder()
-                               .AddOpenAIChatCompletion(modelId: modelPhi3, endpoint: new Uri(endpointOllama), apiKey: null, serviceId: modelPhi3)
-                               ;
+                               .AddOpenAIChatCompletion(modelId: configValues.ModelId, endpoint: new Uri(configValues.Endpoint), apiKey: configValues.ApiKey,
+                               serviceId: configValues.ServiceId);
 
 var kernelBuilderPhi = Kernel.CreateBuilder()
-                             .AddOpenAIChatCompletion(modelId: modelPhi3, endpoint: new Uri(endpointOllama), apiKey: null, serviceId: modelPhi3)
-                             ;
+                             .AddOpenAIChatCompletion(modelId: configValues.ModelId, endpoint: new Uri(configValues.Endpoint), apiKey: configValues.ApiKey,
+                              serviceId: configValues.ServiceId);
 
 var kernelMistral = kernelBuilderMistral.Build();
 var kernelLlama = kernelBuilderLlama.Build();
 var kernelPhi = kernelBuilderPhi.Build();
 
 
-async Task CallMistralAsync(string mission, CancellationToken cancellationToken)
+
+async Task CallModelAsync(Kernel activeModel, string modelSystemPrompt, string mission, CancellationToken cancellationToken)
 {
-    var chatService = kernelMistral.GetRequiredService<IChatCompletionService>();
+    var chatService = activeModel.GetRequiredService<IChatCompletionService>();
 
     var chatHistory = new ChatHistory();
-    chatHistory.AddSystemMessage("profile");
+    chatHistory.AddSystemMessage(modelSystemPrompt);
     chatHistory.AddUserMessage(mission);
 
-    var result = chatService.GetStreamingChatMessageContentsAsync(chatHistory, kernel: kernelMistral, cancellationToken: cancellationToken);
+    var result = chatService.GetStreamingChatMessageContentsAsync(chatHistory, kernel: activeModel, cancellationToken: cancellationToken);
 
     await foreach (var message in result)
     {
@@ -44,20 +58,43 @@ async Task CallMistralAsync(string mission, CancellationToken cancellationToken)
 }
 
 var userInput = string.Empty;
+CancellationTokenSource cts = new();
+CancellationToken cancellationToken = cts.Token;
 
 while (!userInput.Equals(@"bye", StringComparison.OrdinalIgnoreCase))
 {
-    // Render menu
 
-    // Get user mission
+    Console.WriteLine("Welcome aboard the Nautilus submarine, the esteemed abode of the List of Extraordinary AIs. We take upon ourselves the most arduous of missions, and it is not uncommon for us to rescue the world once or twice a week. With which of our distinguished agents do you wish to make acquaintance?");
 
-    // Process user mission
+    Console.WriteLine("1. Lady Mistral");
+    Console.WriteLine("2. Mister Llama");
+    Console.WriteLine("3. The increcible Phi-3");
 
-    // Show result
+    Console.WriteLine("Do not forget to say bye for leaving");
+
+    userInput = Console.ReadLine();
+    
+    switch (userInput)
+    {
+        case "1":
+            Console.WriteLine("Good day, this is Lady Mistral speaking. How may I assist you today?");
+            userInput = Console.ReadLine();
+            await CallModelAsync(kernelMistral, promptsValues.Mistral, userInput, cancellationToken);
+        break;
+        case "2":
+            Console.WriteLine("Good day, this is Mister Llama speaking, Intelligence Manager. What is the mission?");
+            userInput = Console.ReadLine();
+            await CallModelAsync(kernelLlama, promptsValues.Llama, "", cancellationToken);
+        break;
+        case "3":
+            Console.WriteLine("You've reached Phi-3. Speak swiftly and state your purpose.");
+            userInput = Console.ReadLine();
+            await CallModelAsync(kernelPhi,  promptsValues.Phi3, "", cancellationToken);
+        break;
+           
+    }
 
 }
-
-// Render Menu
 
 
 #pragma warning restore SKEXP0050
